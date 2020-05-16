b335cfe3024b80c332da5633b6e9bb982ce02c3b
thiennd3589 <64134085+thiennd3589@users.noreply.github.com>
2020-05-16 08:52:49 +0700

    Update firebase.utils.js

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,14 +4,7 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCIJl2tgxN9IZ_BkK0rxk1s3mz7SS9J5rY",
-    authDomain: "mialbums.firebaseapp.com",
-    databaseURL: "https://mialbums.firebaseio.com",
-    projectId: "mialbums",
-    storageBucket: "mialbums.appspot.com",
-    messagingSenderId: "905260544089",
-    appId: "1:905260544089:web:2b8c79ae6b56af30f50148",
-    measurementId: "G-BDV2KV8NEK"
+    YOUR_CONFIG
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
